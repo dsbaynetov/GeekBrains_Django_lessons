e808b16fe26e00019de3ffdf7a41775c5e5ee0e3
dsbaynetov <baynet@mail.ru>
2019-08-25 14:49:21 +1000

    ДЗ5

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -22,10 +22,12 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('', mainapp.main, name='main'),
-    path('products/', mainapp.products, name='products'),
+#    path('products/', mainapp.products, name='products'),
+    path('products/', include('mainapp.urls', namespace='products')),
     path('contacts/', mainapp.contacts, name='contacts'),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('admin/', admin.site.urls),
+    path('basket/', include('basketapp.urls', namespace='basket')),
 ]
 
 if settings.DEBUG:
