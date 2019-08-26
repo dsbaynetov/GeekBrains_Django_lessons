b9c0d52514b7cb9b836c62e6199f486335928c36
dsbaynetov <baynet@mail.ru>
2019-08-26 22:38:17 +1000

    ДЗ 6

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -10,30 +10,46 @@ def main(request):
     return render(request, 'mainapp/index.html', context=content)
 
 
-# def products(request):
-#     categories = ProductCategory.objects.all()
-#     item_products = Product.objects.all()[:10]
-#     content = {'title': 'Каталог', 'products': item_products, 'categories': categories }
-#     return render(request, 'mainapp/products.html', context=content)
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+def get_hot_product():
+    products = Product.objects.filter(is_hot=True).first()
+    return products
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return same_products
 
 def products(request, pk=None):
-    print(f"pk={pk}")
+    #    print(f"pk={pk}")
     title = 'Каталог'
-
-
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-
+    basket = get_basket(request.user)
+    categories = ProductCategory.objects.all()
     if pk:
-        categories = get_object_or_404(ProductCategory, pk=pk)
+        #        categories = get_object_or_404(ProductCategory, pk=pk)
         products_list = Product.objects.filter(category__pk=pk)
     else:
-        categories = ProductCategory.objects.all()
         products_list = Product.objects.all()
 
-    content = {'title': title, 'categories': categories,'products': products_list,'basket':basket}
+    hot_product = get_hot_product()
+
+    content = {'title': title, 'hot_product': hot_product, 'categories': categories, 'products': products_list, 'basket': basket}
     return render(request, 'mainapp/products.html', context=content)
+
+
+def product(request, pk):
+    title = 'продукты'
+    content = {
+        'title': title,
+        'categories': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+    return render(request, 'mainapp/product.html', content)
 
 
 def contacts(request):
