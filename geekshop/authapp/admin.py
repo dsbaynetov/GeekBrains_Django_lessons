6c1b58eac057e1bf8cc6ad1f2a14d129f046f0fc
dsbaynetov <baynet@mail.ru>
2019-09-02 21:29:47 +1000

    ДЗ к уроку 8

--- geekshop/authapp/admin.py
+++ geekshop/authapp/admin.py
@@ -1,4 +1,46 @@
 from django.contrib import admin
 from .models import ShopUser
+from basketapp.models import BasketSlot
+
 # Register your models here.
 admin.site.register(ShopUser)
+
+class BasketSlotInlibe(admin.TabularInline):
+    model = BasketSlot
+    fields = ('product','quantity',)
+    extra = 0 #сколько показывать предзаполненных полей
+
+
+class ShopUserWithBasket(ShopUser):
+    class Meta:
+        verbose_name = 'Пользователь с корзиной'
+        verbose_name_plural = 'Пользователи с корзиной'
+        proxy = True
+
+@admin.register(ShopUserWithBasket)
+
+class ShopUserWithBasketAdmin(admin.ModelAdmin):
+    list_display = ('username', 'get_basket_quantity', 'get_basket_cost')
+    fields = ('username',)
+    readonly_fields = ('username',)
+    inlines = (BasketSlotInlibe,)
+
+    #переороеделим для показа только пользователей у кого есть корзина
+    def get_queryset(self, request):
+        queryset = super(ShopUserWithBasketAdmin, self ).get_queryset(request)
+        queryset = queryset.filter(basket__quantity__gt=0).distinct()
+        return queryset
+
+    #поля вычисляемые, отсутсвующие в таблицах
+    def get_basket_quantity(self, instance):
+        basket = instance.basket.all()
+        total_quantity = sum(list(map(lambda basket_slot: basket_slot.total_quantity, basket)))
+        return total_quantity
+
+    def get_basket_cost(self, instance):
+        basket = instance.basket.all()
+        total_cost = sum(list(map(lambda basket_slot: basket_slot.total_cost, basket)))
+        return total_cost
+
+    get_basket_quantity.short_description = 'Товаров в корзине'
+    get_basket_cost.short_description = 'Стоимость в корзине'
--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from mainapp.models import Product
 
 
-class Basket(models.Model):
+class BasketSlot(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
@@ -18,17 +18,17 @@ class Basket(models.Model):
     @property
     def total_quantity(self):
         "return total quantity for user"
-        _items = Basket.objects.filter(user=self.user)
+        _items = BasketSlot.objects.filter(user=self.user)
         _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-        print(_totalquantity)
-        print(f"_totalquantity={_totalquantity}")
+        # print(_totalquantity)
+        # print(f"_totalquantity={_totalquantity}")
         return _totalquantity
 
 
     @property
     def total_cost(self):
         "return total cost for user"
-        _items = Basket.objects.filter(user=self.user)
+        _items = BasketSlot.objects.filter(user=self.user)
         _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
         return _totalcost
 
